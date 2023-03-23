8eb3566893d3ee4eaadbac019f0e1ec36929e297
liuzhen88 <1458861320@qq.com>
2023-03-23 13:43:02 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var moment = require("moment");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(moment().format("YYYY-MM-DD HH:mm:ss"))
   res.render('index', { title: 'Express' });
 });
 
 router.post("/", (req, res, next) => {
+  console.log("==============request===================", moment().format("YYYY-MM-DD HH:mm:ss"))
   console.log(req.body);
+  if(req.body.params) {
+    console.log(req.body.params.events);
+  }
   res.send("ok");
 });
 
